583d76efe20ab7d92bf8476df3e92befe4918485
Irvinggg23 <irvinmontes2001@gmail.com>
2024-12-10 06:44:17 -0700

    fix: build

--- app/(routes)/components/CustomersTable/columns-cust.tsx
+++ app/(routes)/components/CustomersTable/columns-cust.tsx
@@ -1,14 +1,14 @@
 "use client"
 
 import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react"
-import { Company } from "@prisma/client"
+import {  Contact } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
 import { Button } from "@/components/ui/button"
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from '@/components/ui/dropdown-menu'
 import Link from "next/link"
 import Image from 'next/image'
 
-export const columns: ColumnDef<Company>[] = [
+export const columns: ColumnDef<Contact>[] = [
     {
       accessorKey: "role",
       header: "Numero de control",
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,33 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import CardSummary from "./components/CardSummary/CardSummary";
-import { BookOpenCheck, UsersRound, Waypoints } from "lucide-react";
-import { LastCustomers } from "./components/LastCustomers";
-import { Salesdistributor } from "./components/Salesdistributor";
 
-export const dataCardSummary = [
-  {
-  icon: UsersRound,
-  total: "12.450",
-  average: 15,
-  title:"Companies Created",
-  tooltipText: "See all of the companies created"
-  },
-  {
-  icon: Waypoints,
-  total: "86.5",
-  average: 80,
-  title:"Total Revenue",
-  tooltipText: "Se all of the summary"
-  },
-  {
-  icon: BookOpenCheck,
-  total: "$363.95",
-  average: 30,
-  title:"Bounce Rate",
-  tooltipText: "Se all of the Bounce rate"
-  }
-]
+import { LastCustomers } from "./components/LastCustomers";
+
+
 export default function Home() {
   return (
     <div>
